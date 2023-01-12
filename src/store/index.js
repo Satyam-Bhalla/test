7890988b6e73a58e14cdfd5ae2a81fc1911d37a7
Satyam <satyambhalla17@gmail.com>
2023-01-12 11:59:36 +0530

    Move Reducer to separate reducers file/folder

--- src/store/index.js
+++ src/store/index.js
@@ -8,20 +8,6 @@ const initialState = {
 const currentFrameMin = 0;
 
 
-const reducerFnA = (state = initialState, action) => {
-    switch (action.type) {
-        case "NEXT":
-            return ({ currentFrame: state.currentFrame + 1 });
-        case "PREV":
-            return (state.currentFrame > currentFrameMin ? { currentFrame: state.currentFrame - 1 }
-                : { currentFrame: state.currentFrame });
-        case "updateBoxStructure":
-            return ({ ...state, boxStructure: [...state.boxStructure, action.payload] });
-        case "deleteBox":
-            return ({ ...state, boxStructure: state.boxStructure.filter((box) => box.id !== action.payload) });
-        default:
-            return state;
-    }
-};
+
 
 export const store = configureStore({ reducer: { reducerFnA } });
